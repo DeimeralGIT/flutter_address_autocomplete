d1489112c56130613c9786db0b72ebb6eead7e6a
DeimeralGIT <93777577+DeimeralGIT@users.noreply.github.com>
2022-07-14 21:35:41 +0400

    Update dropdown_search.dart

--- lib/dropdown_search.dart
+++ lib/dropdown_search.dart
@@ -515,7 +515,7 @@ class DropdownSearchState<T> extends State<DropdownSearch<T>> {
         _selectedItemAsString(getSelectedItem),
         maxLines: maxLines,
         overflow: TextOverflow.ellipsis,
-        style: CWTextStyle(CWTextTypes.inputText, context),,
+        style: TextStyle(CWTextTypes.inputText, context),
           ),
         padding: widget.selectedItemTextPadding??EdgeInsets.zero,
       );
